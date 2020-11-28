afc8f769275a25ae848940bb544c3cfd5e9f9aa2
joshua410RB <joshua.lim19@imperial.ac.uk>
2020-11-28 09:17:23 -0800

    Added bus memory interface

---
+++ rtl/mem_bus/harvard_to_avalon.v
@@ -0,0 +1,180 @@
+module harvard_to_avalon(
+    input logic clk,
+    input logic rst,
+    output logic stall,
+
+    //Harvard Data IOs
+    input logic[31:0] ip_address,   //Instruction port
+    input logic read_ip,
+    output logic[31:0] ip_data,
+
+    input logic[31:0] dp_address,   //Data port
+    input logic[31:0] writedata,
+    input logic[3:0] byteenable,
+    input logic read_dp,
+    input logic write_dp,
+    output logic[31:0] dp_data,
+
+    //Avalon IOs
+    input logic[31:0] avl_readdata,
+    input logic avl_waitrequest,
+    output logic[31:0] avl_address,
+    output logic[3:0] avl_byteenable,
+    output logic[31:0] avl_writedata,
+    output logic avl_read,
+    output logic avl_write
+);
+    typedef enum logic[2:0] {
+        IDLE = 3'b000,
+        INSTR = 3'b011,
+        DATA = 3'b111,
+        CLEAR = 3'b101,
+        ISET = 3'b110,
+        CHILL = 3'b001
+    } state_t;
+
+    //avalon bus controller wires
+    logic[31:0] bus_address;
+    logic[31:0] bus_writedata;
+    logic[3:0] bus_byteenable;
+    logic bus_read;
+    logic bus_write;
+    logic[31:0] bus_readdata;
+    logic bus_busy;
+
+    logic[2:0] state;
+
+    initial begin
+        state=IDLE;
+        bus_address=0;
+        bus_writedata=0;
+        bus_byteenable=4'b1111;
+        bus_read=0;
+        bus_write=0;
+        ip_data=0;
+        dp_data=0;
+        stall=0;
+    end
+
+    always_comb begin
+        if(state==IDLE) begin
+            stall=(read_dp^write_dp) | read_ip;
+            bus_address = (read_dp^write_dp) ? dp_address : ip_address;
+            bus_writedata = (read_dp^write_dp) ? writedata : 0;
+            bus_byteenable = (read_dp^write_dp) ? byteenable : 4'b1111;
+            bus_read = (read_dp^write_dp) ? read_dp : read_ip;
+            bus_write = (read_dp^write_dp) ? write_dp : 0;
+        end
+        else if(state==INSTR) begin
+            stall=1;            
+            bus_address=ip_address;
+            bus_writedata=0;
+            bus_byteenable=4'b1111;
+            bus_read=1;
+            bus_write=0;
+        end
+        else if(state==DATA) begin
+            stall=1;
+            bus_address=dp_address;
+            bus_writedata=writedata;
+            bus_byteenable=byteenable;
+            bus_read=read_dp;
+            bus_write=write_dp;
+        end
+        else if(state==CLEAR) begin
+            stall=1;
+            bus_address=ip_address;
+            bus_writedata=0;
+            bus_byteenable=4'b1111;
+            bus_read=0;
+            bus_write=0;
+        end
+        else if(state==ISET) begin
+            stall=1;
+            bus_address=ip_address;
+            bus_writedata=0;
+            bus_byteenable=4'b1111;
+            bus_read=1;
+            bus_write=0;
+        end
+        else if(state==CHILL) begin
+            stall=0;
+            bus_address = (read_dp^write_dp) ? dp_address : ip_address;
+            bus_writedata = (read_dp^write_dp) ? writedata : 0;
+            bus_byteenable = (read_dp^write_dp) ? byteenable : 4'b1111;
+            bus_read=0;
+            bus_write=0;
+        end
+
+    end
+
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            state=IDLE;
+            bus_address=0;
+            bus_writedata=0;
+            bus_byteenable=4'b1111;
+            bus_read=0;
+            bus_write=0;
+            ip_data=0;
+            dp_data=0;
+            stall=0;
+        end
+        else begin
+            if(state==IDLE) begin
+                if(read_dp^write_dp) begin
+                    state<=DATA;
+                end
+                else if(read_ip) begin
+                    state<=INSTR;
+                end
+            end
+            else if(state==INSTR) begin
+                if(!bus_busy) begin
+                    ip_data<=bus_readdata;
+                    state<=CHILL;
+                end
+            end
+            else if(state==DATA) begin
+                if(!bus_busy) begin
+                    dp_data<=bus_readdata;
+                    if(read_ip) begin
+                        state<=CLEAR;
+                    end
+                    else begin
+                        state<=CHILL;
+                    end
+                end
+            end
+            else if(state==CLEAR) begin
+                state<=ISET;
+            end
+            else if(state==ISET) begin
+                state<=INSTR;
+            end
+            else if(state==CHILL) begin
+                state<=IDLE;
+            end
+        end
+    end
+
+    //////// bus controller /////////////////////////////////////////////
+    avl_master_bc bus_con(
+        clk,
+        rst,
+        avl_readdata,
+        avl_waitrequest,
+        avl_address,
+        avl_byteenable,
+        avl_writedata,
+        avl_read,
+        avl_write,
+        bus_address,
+        bus_writedata,
+        bus_byteenable,
+        bus_read,
+        bus_write,
+        bus_readdata,
+        bus_busy
+    );
+endmodule
---
+++ rtl/mem_harvard/mem_harvard.v
@@ -0,0 +1,116 @@
+module mem_harvard(     //async output, readdata output maintains value when read is not asserted
+    input logic clk,
+    input logic rst,
+    
+    //Instruction port
+    input logic[31:0] ip_address,
+    input logic read_ip,
+    output logic[31:0] ip_data,
+
+    //Data port
+    input logic[31:0] dp_address,
+    input logic[31:0] writedata,
+    input logic[3:0] byteenable,
+    input logic read_dp,
+    input logic write_dp,
+    output logic[31:0] dp_data,
+
+    output logic stall
+);
+
+    parameter INSTR_INIT_FILE = "";
+    parameter DATA_INIT_FILE = "";
+    parameter BLOCK_SIZE = 8192;
+
+    logic[7:0] block1 [BLOCK_SIZE-1:0];   //data block
+    logic[7:0] block2 [BLOCK_SIZE-1:0];  //instruction block
+    logic[31:0] tmp_idata;
+    logic[31:0] tmp_ddata;
+
+    wire[7:0] ti0,ti1,ti2,ti3;
+    wire[7:0] td0,td1,td2,td3;
+    wire ben0, ben1, ben2, ben3;
+    wire[31:0] ip_offset_address;
+
+    initial begin
+        integer i;
+        for(i=0;i<BLOCK_SIZE;i++) begin   //init values set to non-zero for testing, remember to change them back to 0
+            block1[i]=0;
+            block2[i]=0;
+        end
+        if(INSTR_INIT_FILE!="") begin
+            $display("loading instruction mem with %s",INSTR_INIT_FILE);
+            $readmemh(INSTR_INIT_FILE,block2);
+        end
+        if(DATA_INIT_FILE!="") begin
+            $display("loading data mem with %s",DATA_INIT_FILE);
+            $readmemh(DATA_INIT_FILE,block1);
+        end
+        tmp_idata=0;
+        tmp_ddata=0;
+        stall=0;      
+    end
+
+    assign ip_offset_address = ip_address-32'hbfc00000;
+
+    assign ben0 = byteenable[0];
+    assign ben1 = byteenable[1];
+    assign ben2 = byteenable[2];
+    assign ben3 = byteenable[3];
+
+    assign ti0 = tmp_idata[7:0];
+    assign ti1 = tmp_idata[15:8];
+    assign ti2 = tmp_idata[23:16];
+    assign ti3 = tmp_idata[31:24];
+
+    assign td0 = tmp_ddata[7:0];
+    assign td1 = tmp_ddata[15:8];
+    assign td2 = tmp_ddata[23:16];
+    assign td3 = tmp_ddata[31:24];
+
+    always_comb begin
+        ip_data[7:0] = read_ip ? block2[ip_offset_address] : ti0;
+        ip_data[15:8] = read_ip ? block2[ip_offset_address+1] : ti1;
+        ip_data[23:16] = read_ip ? block2[ip_offset_address+2] : ti2;
+        ip_data[31:24] = read_ip ? block2[ip_offset_address+3] : ti3;
+
+        dp_data[7:0] = read_dp ? (ben0 ? block1[dp_address] : 8'h00) : td0;
+        dp_data[15:8] = read_dp ? (ben1 ? block1[dp_address+1] : 8'h00) : td1;
+        dp_data[23:16] = read_dp ? (ben2 ? block1[dp_address+2] : 8'h00) : td2;
+        dp_data[31:24] = read_dp ? (ben3 ? block1[dp_address+3] : 8'h00) : td3;        
+    end
+
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            tmp_idata=0;
+            tmp_ddata=0;
+            stall=0;
+        end
+        else begin
+            if(read_ip==1) begin
+                tmp_idata[7:0]<=block2[ip_offset_address];
+                tmp_idata[15:8]<=block2[ip_offset_address+1];
+                tmp_idata[23:16]<=block2[ip_offset_address+2];
+                tmp_idata[31:24]<=block2[ip_offset_address+3];
+            end
+            if(write_dp==1) begin
+                block1[dp_address] <= ben0 ? writedata[7:0] : block1[dp_address];
+                block1[dp_address+1] <= ben1 ? writedata[15:8] : block1[dp_address+1];
+                block1[dp_address+2] <= ben2 ? writedata[23:16] : block1[dp_address+2];
+                block1[dp_address+3] <= ben3 ? writedata[31:24] : block1[dp_address+3];
+
+                tmp_ddata[7:0] <= ben0 ? writedata[7:0] : tmp_ddata[7:0];
+                tmp_ddata[15:8] <= ben1 ? writedata[15:8] : tmp_ddata[15:8];
+                tmp_ddata[23:16] <= ben2 ? writedata[23:16] : tmp_ddata[23:16];
+                tmp_ddata[31:24] <= ben3 ? writedata[31:24] : tmp_ddata[31:24];
+            end
+            else if(read_dp==1) begin
+                tmp_ddata[7:0]<=block1[dp_address];
+                tmp_ddata[15:8]<=block1[dp_address+1];
+                tmp_ddata[23:16]<=block1[dp_address+2];
+                tmp_ddata[31:24]<=block1[dp_address+3];
+            end
+        end
+    end
+    
+endmodule
