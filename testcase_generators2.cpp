c862d5a4dd9fe36294360be66023ea777e02e782
aq119 <aq119@ic.ac.uk>
2020-11-30 15:52:09 +0800

    modified testcase_generator

--- testcase_generators2.cpp
+++ testcase_generators2.cpp
@@ -14,22 +14,18 @@ string assign_reg(int n, string side, int R)
 { // 1 == ++, 2 == +-,3 == -+, 4 == --
     string res;
     string r = to_string(R);
-    assert(n == 1 || n == 2 || n == 3 || n == 4);
+    assert(n == 1 || n == 2);
     assert(side == "upper" || side == "lower");
 
-    if (side == "upper" && (n == 1 || n == 2))
+    if (side == "upper")
     {
-        res = "lui $s" + r + "," + to_string(rand() % 32767);
+        res = "lui $s" + r + "," + to_string(rand() % 131072);
     }
-    if (side == "upper" && (n == 3 || n == 4))
-    {
-        res = "lui $s" + r + "," + to_string((rand() % 32767) * -1 - 1);
-    }
-    if (side == "lower" && (n == 1 || n == 3))
+    if (side == "lower" && n == 1)
     {
         res = "addiu $s" + r + ",$" + r + "," + to_string(rand() % 32767);
     }
-    if (side == "lower" && (n == 2 || n == 4))
+    if (side == "lower" && n == 2)
     {
         res = "addiu $s" + r + ",$" + r + "," + to_string((rand() % 32767) * -1 - 1);
     }
@@ -44,112 +40,87 @@ int main()
     string position = "test/0-assembly/"; //storing positionition for the testcases txt
     ofstream outfile;
 
-    int num = 1;
-
-    for (int i = 0; i < num; i++)
+    if (instr == "lui") //this instruction should be tested first
+    // the immediate should not be signed, so only positive value allowed
     {
-        if (instr == "lui") //this instruction should be tested first
-        {
-            outfile.open(position + instr + "_1.asm.txt", ios::trunc);
-            outfile << instr << " $v0," << rand() % 32767 << endl;
-            outfile.close();
-            outfile.open(position + instr + "_2.asm.txt", ios::trunc);
-            outfile << instr << " $v0," << ((rand() % 32767) * -1 - 1) << endl;
-            outfile.close();
-        }
+        outfile.open(position + instr + "_.asm.txt", ios::trunc);
+        outfile << instr << " $v0," << rand() % 131072 << endl;
+        outfile.close();
+    }
 
-        if (instr == "addiu")
-        { //1.+++,2.+-+,3.-++,4.--+, 5.++-,6.+--,7.-+-,8,---
-            for (int i = 1; i <= 4; i++)
-            {
-                int r1 = rand() % 7;
-                outfile.open(position + instr + "_" + to_string(i) + ".asm.txt", ios::trunc);
-                outfile << assign_reg(i, "upper", r1) << endl;
-                outfile << assign_reg(i, "lower", r1) << endl;
-                outfile << instr << " $s" << r1 << ",$s" << r1 << "," << rand() % 32767 << endl;
-                outfile << "addiu $v0,$s" << r1 << ",0" << endl;
-                outfile.close();
-                outfile.open(position + instr + "_" + to_string(i + 4) + ".asm.txt", ios::trunc);
-                outfile << assign_reg(i, "upper", r1) << endl;
-                outfile << assign_reg(i, "lower", r1) << endl;
-                outfile << instr << " $s" << r1 << ",$s" << r1 << "," << (rand() % 32767) * -1 - 1 << endl;
-                outfile << "addiu $v0,$s" << r1 << ",0" << endl;
-                outfile.close();
-            }
-        }
-
-        if (instr == "ori" || instr == "andi")
+    if (instr == "addiu")
+    {   //
+        //the immediate here should be signed
+        for (int i = 1; i <= 2; i++)
         {
-            int c1 = rand() % 3 + 1;
-            int c2 = rand() % 3 + 1;
             int r1 = rand() % 7;
-            outfile.open(position + instr + "_1.asm.txt", ios::trunc);
-            outfile << assign_reg(c1, "upper", r1) << endl;
-            outfile << assign_reg(c1, "lower", r1) << endl;
+            outfile.open(position + instr + "_" + to_string(i) + ".asm.txt", ios::trunc);
+            outfile << assign_reg(i, "upper", r1) << endl;
+            outfile << assign_reg(i, "lower", r1) << endl;
             outfile << instr << " $s" << r1 << ",$s" << r1 << "," << rand() % 32767 << endl;
             outfile << "addiu $v0,$s" << r1 << ",0" << endl;
             outfile.close();
-            outfile.open(position + instr + "_2.asm.txt", ios::trunc);
-            outfile << assign_reg(c2, "upper", r1) << endl;
-            outfile << assign_reg(c2, "lower", r1) << endl;
+            outfile.open(position + instr + "_" + to_string(i + 2) + ".asm.txt", ios::trunc);
+            outfile << assign_reg(i, "upper", r1) << endl;
+            outfile << assign_reg(i, "lower", r1) << endl;
             outfile << instr << " $s" << r1 << ",$s" << r1 << "," << (rand() % 32767) * -1 - 1 << endl;
             outfile << "addiu $v0,$s" << r1 << ",0" << endl;
             outfile.close();
         }
+    }
 
-        if (instr == "and" || instr == "or")
+    if (instr == "ori" || instr == "andi")
+    {
+        int r1 = rand() % 7;
+        int r2 = rand() % 7;
+        outfile.open(position + instr + "_1.asm.txt", ios::trunc);
+        outfile << assign_reg(1, "upper", r1) << endl;
+        outfile << assign_reg(1, "lower", r1) << endl;
+        outfile << instr << " $s" << r1 << ",$s" << r1 << "," << rand() % 32767 << endl;
+        outfile << "addiu $v0,$s" << r1 << ",0" << endl;
+        outfile.close();
+        outfile.open(position + instr + "_2.asm.txt", ios::trunc);
+        outfile << assign_reg(2, "upper", r2) << endl;
+        outfile << assign_reg(2, "lower", r2) << endl;
+        outfile << instr << " $s" << r2 << ",$s" << r2 << "," << (rand() % 32767) * -1 - 1 << endl;
+        outfile << "addiu $v0,$s" << r2 << ",0" << endl;
+        outfile.close();
+    }
+
+    if (instr == "and" || instr == "or")
+    {
+        int r1 = rand() % 7;
+        int r2 = rand() % 7;
+        outfile.open(position + instr + ".asm.txt", ios::trunc);
+        outfile << assign_reg(1, "upper", r1) << endl;
+        outfile << assign_reg(1, "lower", r1) << endl;
+        outfile << assign_reg(2, "upper", r2) << endl;
+        outfile << assign_reg(2, "lower", r2) << endl;
+        outfile << instr << " $s" << r1 << ",$s" << r1 << ",$s" << r2 << endl;
+        outfile << "addiu $v0,$s" << r1 << ",0" << endl;
+        outfile.close();
+    }
+
+    if (instr == "addu" || instr == "div" || instr == "divu" || instr == "mult" || instr == "multu")
+    {
+        for (int i = 1; i <= 4; i++)
         {
+            //11,12,21,22
+            outfile.open(position + instr + "_" + to_string(i) + ".asm.txt", ios::trunc);
             int r1 = rand() % 7;
             int r2 = rand() % 7;
-            int c1 = rand() % 3 + 1;
-            int c2 = rand() % 3 + 1;
-            outfile.open(position + instr + ".asm.txt", ios::trunc);
-            outfile << assign_reg(c1, "upper", r1) << endl;
+            int c1,c2;
+            if(i == 1){c1 = c2 = 1;}
+            if(i == 2){c1 = 1; c2 = 2;}
+            if(i == 3){c1= 2;c2 = 1;}
+            if(i == 4){c1 = c2 = 2;}
+            outfile << assign_reg(1, "upper", r1) << endl;
             outfile << assign_reg(c1, "lower", r1) << endl;
-            outfile << assign_reg(c2, "upper", r2) << endl;
+            outfile << assign_reg(1, "upper", r2) << endl;
             outfile << assign_reg(c2, "lower", r2) << endl;
             outfile << instr << " $s" << r1 << ",$s" << r1 << ",$s" << r2 << endl;
             outfile << "addiu $v0,$s" << r1 << ",0" << endl;
             outfile.close();
-        }
-
-        if (instr == "addu" || instr == "div" || instr == "divu" || instr == "mult" || instr == "multu")
-        {
-            for (int i = 1; i <= 4; i++)
-            {
-                outfile.open(position + instr + "_" + to_string(i) + ".asm.txt", ios::trunc);
-                int c1;
-                int c2;
-                if (i == 1)
-                {
-                    c1 = rand() % 2 + 1;
-                    c2 = rand() % 2 + 1;
-                }
-                else if (i == 2)
-                {
-                    c1 = rand() % 2 + 1;
-                    c2 = rand() % 2 + 3;
-                }
-                else if (i == 3)
-                {
-                    c1 = rand() % 2 + 3;
-                    c2 = rand() % 2 + 1;
-                }
-                else
-                {
-                    c1 = rand() % 2 + 3;
-                    c2 = rand() % 2 + 3;
-                }
-                int r1 = rand() % 7;
-                int r2 = rand() % 7;
-                outfile << assign_reg(c1, "upper", r1) << endl;
-                outfile << assign_reg(c1, "lower", r1) << endl;
-                outfile << assign_reg(c2, "upper", r2) << endl;
-                outfile << assign_reg(c2, "lower", r2) << endl;
-                outfile << instr << " $s" << r1 << ",$s" << r1 << ",$s" << r2 << endl;
-                outfile << "addiu $v0,$s" << r1 << ",0" << endl;
-                outfile.close();
-            }
         }
     }
 }
