5c339f04c515a6c79804d6d5779d10d6a40eed5c
QAAQQQ <42152083+QAAQQQ@users.noreply.github.com>
2020-12-02 02:41:00 +0800

    Update no_init_test_mips_cpu.sh

--- test/no_init_test_mips_cpu.sh
+++ test/no_init_test_mips_cpu.sh
@@ -6,7 +6,7 @@ set -eou pipefail
 ADR="$1"
 if [ 2 -eq "$#" ];then
     #run all test cases for $2 particular instr
-    TESTCASES="../test/0-assembly/$2*.asm.txt"
+    TESTCASES="test/0-assembly/$2*.asm.txt"
     INSTR="$2"
     for i in ${TESTCASES} ; do
     # Extract just the testcase name from the filename. See `man basename` for what this command does.
@@ -23,14 +23,14 @@ if [ 2 -eq "$#" ];then
 
 else
     #first run the 5 basic test cases that needed to be tested
-    ./no_init_test_mips_cpu.sh ${ADR} "lui"
-    ./no_init_test_mips_cpu.sh ${ADR} "addiu"
-    ./no_init_test_mips_cpu.sh ${ADR} "lw"
-    ./no_init_test_mips_cpu.sh ${ADR} "sw"
-    ./no_init_test_mips_cpu.sh ${ADR} "jr"
+    ./test/no_init_test_mips_cpu.sh ${ADR} "lui"
+    ./test/no_init_test_mips_cpu.sh ${ADR} "addiu"
+    ./test/no_init_test_mips_cpu.sh ${ADR} "lw"
+    ./test/no_init_test_mips_cpu.sh ${ADR} "sw"
+    ./test/no_init_test_mips_cpu.sh ${ADR} "jr"
 
     #run all testcase for all test cases
-    TESTCASES="../test/0-assembly/*.asm.txt"
+    TESTCASES="test/0-assembly/*.asm.txt"
     for i in ${TESTCASES} ; do
     # Extract just the testcase name from the filename. See `man basename` for what this command does.
     TESTNAME=$(basename ${i} .asm.txt)
@@ -39,7 +39,7 @@ else
     # Dispatch to the main test-case script
     if [ ${INSTR} != 'lui' ] && [ ${INSTR} != 'addiu' ] && [ ${INSTR} != 'lw' ] && [ ${INSTR} != 'sw' ] && [ ${INSTR} != 'jr' ];
     then
-    ./3.sh ${ADR} ${INSTR} ${TESTNAME}
+    ./test_one_instr_mips_cpu_harvard.sh ${ADR} ${INSTR} ${TESTNAME}
     fi
 
     done
