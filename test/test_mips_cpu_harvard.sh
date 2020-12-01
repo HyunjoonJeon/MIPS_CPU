00afd60c9cb39728f803daa167313fc1bb9dde0b
QAAQQQ <42152083+QAAQQQ@users.noreply.github.com>
2020-12-02 02:38:10 +0800

    Update test_mips_cpu_harvard.sh

--- test/test_mips_cpu_harvard.sh
+++ test/test_mips_cpu_harvard.sh
@@ -4,17 +4,17 @@ set -eou pipefail
 # can be used for both harvard and bus tests
 
 >&2 echo "Checking if initialisation required..."
-if [ -e ../bin/tc ];then
+if [ -e bin/tc ] && [ -e bin/assembler ] && [ -e bin/reference ] ;then
 >&2 echo "......Done"
 else
 >&2 echo "Initialising"
-./generate_bin.sh
+./test/generate_bin.sh
 >&2 echo "......Done"
 fi
 
 >&2 echo "Test CPU In Folder: $1"
 if [ 2 -eq "$#" ];then
-    ./no_init_test_mips_cpu.sh $1 $2
+    ./test/no_init_test_mips_cpu.sh $1 $2
 else
-    ./no_init_test_mips_cpu.sh $1
+    ./test/no_init_test_mips_cpu.sh $1
 fi
