e567e04c05b6eb281ca4e3baec039c03bf3afb3f
QAAQQQ <42152083+QAAQQQ@users.noreply.github.com>
2020-12-02 02:34:33 +0800

    Update generate_bin.sh

--- test/generate_bin.sh
+++ test/generate_bin.sh
@@ -1,20 +1,20 @@
-if [ ! -d ../bin ];then
+if [ ! -d bin ];then
 #if bin directory not exist, create one
-mkdir ../bin
+mkdir bin
 fi
 #Generate excutable program in bin from cpp files in util
-g++ ../util/testcase_generators.cpp -o ../bin/tc
-g++ ../util/assembler.cpp -o ../bin/assembler
-g++ ../util/reference_generator.cpp -o ../bin/ref
-echo "lui" | ../bin/tc 
-echo "addiu" | ../bin/tc
-echo "addu" | ../bin/tc 
-echo "and" | ../bin/tc 
-echo "and" | ../bin/tc 
-echo "andi" | ../bin/tc  
-echo "div" | ../bin/tc 
-echo "divu" | ../bin/tc 
-echo "mult" | ../bin/tc 
-echo "multu" | ../bin/tc 
-echo "or" | ../bin/tc 
-echo "ori" | ../bin/tc 
+g++ util/testcase_generators.cpp -o bin/tc
+g++ util/assembler.cpp -o bin/assembler
+g++ util/reference_generator.cpp -o bin/ref
+echo "lui" | bin/tc 
+echo "addiu" | bin/tc
+echo "addu" | bin/tc 
+echo "and" | bin/tc 
+echo "and" | bin/tc 
+echo "andi" | bin/tc  
+echo "div" | bin/tc 
+echo "divu" | bin/tc 
+echo "mult" | bin/tc 
+echo "multu" | bin/tc 
+echo "or" | bin/tc 
+echo "ori" | bin/tc 
