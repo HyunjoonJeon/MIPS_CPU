80dc9cb101039d7a22c5b027231ccb56df0f7625
sy1819 <sy1819@ic.ac.uk>
2020-12-03 16:10:14 +0000

    testbench for harvard done

--- rtl/mips_cpu_harvard_tb.v
+++ rtl/mips_cpu_harvard_tb.v
@@ -1,7 +1,7 @@
 module mips_cpu_harvard_tb;
     timeunit 1ns / 100ps; //time unit and precision
 
-    parameter INSTR_INIT_FILE = "test/1-binary/addiu/addiu1.hex.txt";
+    parameter INSTR_INIT_FILE = "test/1-binary/addiu/addiu_1.hex.txt";
     parameter DATA_INIT_FILE = "";
     parameter TIMEOUT_CYCLES = 40;
 
@@ -59,7 +59,7 @@ module mips_cpu_harvard_tb;
         @(negedge active);
 
         $display("TB : finished; active=0");
-        $display("register_v0:%d", register_v0);
+        $display("register_v0:%h", register_v0);
     
         $finish;
         
--- test/generate_bin.sh
+++ test/generate_bin.sh
@@ -3,9 +3,9 @@ if [ ! -d bin ];then
 mkdir bin
 fi
 #Generate excutable program in bin from cpp files in util
-g++ util/testcase_generators.cpp -o bin/tc
-g++ util/assembler.cpp -o bin/assembler
-g++ util/reference_generator.cpp -o bin/ref
+#g++ utils/testcase_generators.cpp -o bin/tc
+#g++ utils/assembler.cpp -o bin/assembler
+#g++ utils/reference_generator.cpp -o bin/ref
 echo "lui" | bin/tc 
 echo "addiu" | bin/tc
 echo "addu" | bin/tc 
