92feff4b71c62f9aba94c7d508b8f23414f50180
joshua410RB <joshua.lim19@imperial.ac.uk>
2020-12-02 19:02:17 -0800

    cleaned up debug code from harvard CPU test bench

--- rtl/mips_cpu_harvard_tb.v
+++ rtl/mips_cpu_harvard_tb.v
@@ -26,7 +26,6 @@ module mips_cpu_harvard_tb;
     integer counter;
 
     mem_harvard #(.INSTR_INIT_FILE(INSTR_INIT_FILE), .DATA_INIT_FILE(DATA_INIT_FILE), .BLOCK_SIZE(8192)) instrInst(.clk(clk), .rst(rst), .ip_address(instr_address), .read_ip(instr_read), .ip_readdata(instr_readdata),.dp_address(data_address), .writedata(data_writedata), .byteenable(byte_enable), .read_dp(data_read), .write_dp(data_write)); //input the INSTR_INIT_FILE
-    // mem_harvard #(DATA_INIT_FILE) dataInst(.clk(clk), .rst(rst), .ip_address(instr_address), .read_ip(instr_read), .dp_address(data_address), .writedata(data_writedata), .byteenable(byte_enable), .read_dp(data_read), .write_dp(data_write), .dp_readdata(data_readdata)); 
     mips_cpu_harvard cpuInst(.clk(clk), .clk_enable(clk_enable), .reset(rst), .active(active), .register_v0(register_v0), .instr_readdata(instr_readdata), .instr_address(instr_address), .instr_read(instr_read), .byte_enable(byte_enable), .data_address(data_address), .data_write(data_write), .data_read(data_read), .data_readdata(data_readdata));
 
     // Generate clock
@@ -40,8 +39,6 @@ module mips_cpu_harvard_tb;
             clk = !clk;
             counter++;
         end
-        $display("end instr address: %h", instr_address);
-        $display("end data: %h",instr_readdata);
         $fatal(2, "Simulation did not finish within %d cycles.", TIMEOUT_CYCLES);
     end
 
@@ -49,36 +46,17 @@ module mips_cpu_harvard_tb;
         $display("");
         clk_enable=1;
         rst = 0;
-
-        //test mem
-        // instr_address=32'hbfc00000;
-        // instr_read=1;
         #1;
-        $display("active: %b",active);
 
         @(posedge clk); //reset
         rst = 1;
         @(posedge clk);
-        #1;
-        
-        // @(posedge clk); //start
+        #2;
         rst = 0;
         
-        @(posedge clk);
-
-        $display("instr_address: %h",instr_address);
-        $display("instr data: %h",instr_readdata);
-        $display("read mem: %b",instr_read);
-        $display("active: %b",active);
-        // assert(active==1) //make sure it is running
-        // else $display("TB : CPU did not set running=1 after reset.");
-        #4;
-        $display("probe: %h",data_address);
+        assert(active==1) //make sure it is running
+        else $display("TB : CPU did not set running=1 after reset.");
         @(negedge active);
-
-        // while (active) begin
-        //     @(posedge clk);
-        // end
 
         $display("TB : finished; active=0");
         $display("register_v0:%d", register_v0);
