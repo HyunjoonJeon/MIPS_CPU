d5433ff2c5ece0ab5ca43a9b5633b21894e53bd9
aq119 <aq119@ic.ac.uk>
2020-12-04 19:10:23 +0800

    modified sh2 and change tc sw names

---
+++ test/test_mips_cpu_bus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eou pipefail
+#input is address + optional instruction
+
+>&2 echo "Checking if initialisation required..."
+if [ -e bin/tc ] && [ -e bin/assembler ] && [ -e bin/reference ] ;then
+>&2 echo "......Done"
+else
+>&2 echo "Initialising"
+./test/generate_bin.sh
+>&2 echo "......Done"
+fi
+
+>&2 echo "Test CPU In Folder: $1"
+if [ 2 -eq "$#" ];then
+    ./test/no_init_test_mips_cpu.sh $1 "bus" $2
+else
+    ./test/no_init_test_mips_cpu.sh $1 "bus"
+fi
