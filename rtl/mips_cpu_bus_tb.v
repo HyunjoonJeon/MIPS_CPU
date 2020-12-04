a0426368d5faf9be2ab180ed7331e146171f75c8
joshua410RB <joshua.lim19@imperial.ac.uk>
2020-12-03 22:14:01 -0800

    added testcases for ADDU, SUBU, AND and ANDI

--- rtl/mips_cpu_bus_tb.v
+++ rtl/mips_cpu_bus_tb.v
@@ -1,7 +1,7 @@
 module mips_cpu_harvard_tb;
     timeunit 1ns / 100ps; //time unit and precision
 
-    parameter INSTR_INIT_FILE = "test/1-binary/addiu/addiu_2.hex.txt";
+    parameter INSTR_INIT_FILE = "test/1-binary/subu/subu_2.hex.txt";
     parameter DATA_INIT_FILE = "";
     parameter TIMEOUT_CYCLES = 40;
 
@@ -50,7 +50,7 @@ module mips_cpu_harvard_tb;
         @(negedge active);
 
         $display("TB : finished; active=0");
-        $display("register_v0:%d", register_v0);
+        $display("register_v0:%h", register_v0);
     
         $finish;
         
