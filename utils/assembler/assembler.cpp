f26d337b4f8bc77d7c49f7c3a5b53812ac577176
Raghav Viswakumar <raghavviswakumar@Raghavs-MacBook-Pro.local>
2020-12-04 11:30:58 +0000

    Assembler Bug Fixes to LUI and Immediate

--- utils/assembler/assembler.cpp
+++ utils/assembler/assembler.cpp
@@ -160,7 +160,7 @@ int main(){
                 if(temp2[0] == '0' && temp2[1] == 'x'){
                     cout << temp1.substr(0, 3) + temp2.substr(2) << endl;
                 }
-                cout << temp1.substr(0, 3) + to_hex8(stoi(temp2.substr(0))).substr(4, 8) << endl;
+                cout << temp1.substr(0, 3) + to_hex8(stoi(temp2.substr(0))).substr(4, 7) << endl;
             }else if(mips_instruction_is_branch(opname)){
                 assert(labels.find(instruction_set[i].s3)!=labels.end());
                 uint32_t address=labels[instruction_set[i].s3];
@@ -190,7 +190,10 @@ int main(){
             }else if(opname == "lui"){
                 string temp1 = to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 2));
                 string temp2 = instruction_set[i].s2;
-                cout << temp1.substr(0, 15) + temp2.substr(2) << endl;
+                if(temp2[0] == '0' && temp2[1] == 'x'){
+                    cout << temp1.substr(0, 3) + temp2.substr(2) << endl;
+                }
+                cout << temp1.substr(0, 3) + to_hex8(stoi(temp2.substr(0))).substr(4, 7) << endl;
             }
 
     }
