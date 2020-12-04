d17fa92b503d654a10a80edbadbdcf8f9fe9306a
joshua410RB <joshua.lim19@imperial.ac.uk>
2020-12-03 23:29:36 -0800

    added some test cases for SLL

--- rtl/mips_cpu_bus_tb.v
+++ rtl/mips_cpu_bus_tb.v
@@ -1,9 +1,9 @@
-module mips_cpu_harvard_tb;
+module mips_cpu_bus_tb();
     timeunit 1ns / 100ps; //time unit and precision
 
-    parameter INSTR_INIT_FILE = "test/1-binary/subu/subu_2.hex.txt";
+    parameter INSTR_INIT_FILE = "test/1-binary/sll/sll_3.hex.txt";
     parameter DATA_INIT_FILE = "";
-    parameter TIMEOUT_CYCLES = 40;
+    parameter TIMEOUT_CYCLES = 500;
 
     logic clk;
     logic rst;
