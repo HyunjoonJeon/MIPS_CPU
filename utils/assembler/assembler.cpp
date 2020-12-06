e7759345ffd359ee0e9cb3e3ca6dcd4c2374edf7
Raghav Viswakumar <raghavviswakumar@Raghavs-MacBook-Pro.local>
2020-12-06 23:04:35 +0000

    Fixing shifts sa field

--- utils/assembler/assembler.cpp
+++ utils/assembler/assembler.cpp
@@ -193,7 +193,9 @@ int main(){
             }else if(mips_instruction_is_MulDiv(opname)){
                 cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 1) + mips_regname_to_regcode(instruction_set[i].s2, 2)) << endl;
             }else if(mips_instruction_is_shift(opname)){
-                cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 2) + mips_regname_to_regcode(instruction_set[i].s2, 3) + stoi(instruction_set[i].s3)) << endl;
+                uint32_t temp = stoi(instruction_set[i].s3));
+                uint32_t sa = temp << 6;
+                cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 2) + mips_regname_to_regcode(instruction_set[i].s2, 3) + sa) << endl;
             }else if(mips_instruction_is_shift_variable(opname)){
                 cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 1) + mips_regname_to_regcode(instruction_set[i].s2, 2) + mips_regname_to_regcode(instruction_set[i].s3, 3)) << endl;
             }else if(opname == "jalr"){
