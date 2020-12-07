133f842dd0163fcae518e894df4788d49332dd1f
Raghav Viswakumar <raghavviswakumar@Raghavs-MacBook-Pro.local>
2020-12-07 21:12:10 +0000

    Testing jump with FC000

--- utils/assembler/assembler.cpp
+++ utils/assembler/assembler.cpp
@@ -126,6 +126,7 @@ int main(){
                 string rs;
                 cin >> rd;
                 cin >> rs;
+                assert(!cin.fail());
                 instruction temp = {head, rs, rd, "0"};
                 instruction_set.push_back(temp);
             }else if(head == "jr"){
@@ -181,7 +182,7 @@ int main(){
                 cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 1) + address) << endl;
             }else if(mips_instruction_is_jump(opname)){
                 assert(labels.find(instruction_set[i].s1)!=labels.end());
-                uint32_t address=labels[instruction_set[i].s1];
+                uint32_t address=labels[instruction_set[i].s1]+ 1032192;
                 cout << to_hex8(opcode + address) << endl;
             }else if(mips_instruction_is_memory_using_offset(opname)){
                 cout << to_hex8(opcode + mips_regname_to_regcode(instruction_set[i].s1, 1) + mips_regname_to_regcode(instruction_set[i].s2, 2) + stoi(instruction_set[i].s3)) << endl;
