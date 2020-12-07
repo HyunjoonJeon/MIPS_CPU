97de8aae7631391c3e3222a8f26bd9d5837c4099
sittingsotong <zhaositing1026@gmail.com>
2020-12-07 12:06:54 +0000

    gitignore still not working

--- rtl/mips_cpu/lwlr.v
+++ rtl/mips_cpu/lwlr.v
@@ -11,16 +11,16 @@ module lwlr(
         // if select == 1 means LWL
         case(byte_offset)
         2'b00: begin
-            reg_write_data = lwl ? data_readdata : {reg_data_b[31:8], data_readdata[31:24]};
-        end
+            reg_write_data = lwl ? {data_readdata[7:0], reg_data_b[23:0]} : data_readdata;
+        end        
         2'b01: begin
-            reg_write_data = lwl ? {data_readdata[23:0], reg_data_b[7:0]} : {reg_data_b[31:16], data_readdata[31:16]};
-        end
-        2'b10: begin
             reg_write_data = lwl ? {data_readdata[15:0], reg_data_b[15:0]} : {reg_data_b[31:24], data_readdata[31:8]};
         end
+        2'b10: begin
+            reg_write_data = lwl ? {data_readdata[23:0], reg_data_b[7:0]} : {reg_data_b[31:16], data_readdata[31:16]};
+        end
         2'b11: begin
-            reg_write_data = lwl ? {data_readdata[7:0], reg_data_b[23:0]} : data_readdata;
+            reg_write_data = lwl ? data_readdata : {reg_data_b[31:8], data_readdata[31:24]};
         end
         endcase
     end
