3f26cce84d6c634b35356baf17befa133ccaeacf
aq119 <aq119@ic.ac.uk>
2020-12-09 22:35:32 +0800

    added error comment & update sh

--- test/regenerate_random_tc.sh
+++ test/regenerate_random_tc.sh
@@ -1,7 +1,45 @@
+if [ ! -d test/0-assembly/lui ];then
+mkdir -p test/0-assembly/lui
+fi
+if [ ! -d test/0-assembly/addiu ];then
+mkdir -p test/0-assembly/addiu
+fi
+if [ ! -d test/0-assembly/addu ];then
+mkdir -p test/0-assembly/addu
+fi
+if [ ! -d test/0-assembly/and ];then
+mkdir -p test/0-assembly/and
+fi
+if [ ! -d test/0-assembly/andi ];then
+mkdir -p test/0-assembly/andi
+fi
+if [ ! -d test/0-assembly/div ];then
+mkdir -p test/0-assembly/div
+fi
+if [ ! -d test/0-assembly/divu ];then
+mkdir -p test/0-assembly/divu
+fi
+if [ ! -d test/0-assembly/mult ];then
+mkdir -p test/0-assembly/mult
+fi
+if [ ! -d test/0-assembly/or ];then
+mkdir -p test/0-assembly/or
+fi
+if [ ! -d test/0-assembly/ori ];then
+mkdir -p test/0-assembly/ori
+fi
+if [ ! -d test/0-assembly/xor ];then
+mkdir -p test/0-assembly/xor
+fi
+if [ ! -d test/0-assembly/xori ];then
+mkdir -p test/0-assembly/xori
+fi
+if [ ! -d test/0-assembly/subu ];then
+mkdir -p test/0-assembly/subu
+fi
 echo "lui" |  bin/tc 
 echo "addiu" |  bin/tc
 echo "addu" |  bin/tc 
-echo "and" |  bin/tc 
 echo "and" |  bin/tc 
 echo "andi" |  bin/tc  
 echo "div" |  bin/tc 
