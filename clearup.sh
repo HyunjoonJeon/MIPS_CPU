770fa56d0606f886b041590c32322206448b5cb1
aq119 <aq119@ic.ac.uk>
2020-12-13 21:32:02 +0800

    added clearing up sh

---
+++ clearup.sh
@@ -0,0 +1,60 @@
+rm ./test/0-assembly/addiu/addiu_1.asm.txt
+rm ./test/0-assembly/addiu/addiu_2.asm.txt
+rm ./test/0-assembly/addiu/addiu_3.asm.txt
+rm ./test/0-assembly/addiu/addiu_4.asm.txt
+
+rm ./test/0-assembly/addu/addu_1.asm.txt
+rm ./test/0-assembly/addu/addu_2.asm.txt
+rm ./test/0-assembly/addu/addu_3.asm.txt
+rm ./test/0-assembly/addu/addu_4.asm.txt
+
+rm -r ./test/0-assembly/and*.txt
+mkdir -p ./test/0-assembly/and
+
+rm ./test/0-assembly/andi/andi_1.asm.txt
+rm ./test/0-assembly/andi/andi_2.asm.txt
+
+rm ./test/0-assembly/div/div_1.asm.txt
+rm ./test/0-assembly/div/div_2.asm.txt
+rm ./test/0-assembly/div/div_3.asm.txt
+rm ./test/0-assembly/div/div_4.asm.txt
+rm ./test/0-assembly/div/div_5.asm.txt
+rm ./test/0-assembly/div/div_6.asm.txt
+rm ./test/0-assembly/div/div_7.asm.txt
+rm ./test/0-assembly/div/div_8.asm.txt
+
+
+rm ./test/0-assembly/divu/divu_1.asm.txt
+rm ./test/0-assembly/divu/divu_2.asm.txt
+rm ./test/0-assembly/divu/divu_3.asm.txt
+rm ./test/0-assembly/divu/divu_4.asm.txt
+rm ./test/0-assembly/divu/divu_5.asm.txt
+rm ./test/0-assembly/divu/divu_6.asm.txt
+rm ./test/0-assembly/divu/divu_7.asm.txt
+rm ./test/0-assembly/divu/divu_8.asm.txt
+
+rm ./test/0-assembly/lui/lui*.txt
+
+rm ./test/0-assembly/mult/mult_1.asm.txt
+rm ./test/0-assembly/mult/mult_2.asm.txt
+rm ./test/0-assembly/mult/mult_3.asm.txt
+rm ./test/0-assembly/mult/mult_4.asm.txt
+rm ./test/0-assembly/mult/mult_5.asm.txt
+rm ./test/0-assembly/mult/mult_6.asm.txt
+rm ./test/0-assembly/mult/mult_7.asm.txt
+rm ./test/0-assembly/mult/mult_8.asm.txt
+rm ./test/0-assembly/mult/mult_9.asm.txt
+rm ./test/0-assembly/mult/mult_10.asm.txt
+
+rm ./test/0-assembly/or/or*.txt
+
+rm ./test/0-assembly/ori/ori_1.asm.txt
+rm ./test/0-assembly/ori/ori_2.asm.txt
+
+rm ./test/0-assembly/subu/subu*.txt
+
+rm ./test/0-assembly/xor/xor*.txt
+
+rm ./test/0-assembly/xori/xori_1.asm.txt
+rm ./test/0-assembly/xori/xori_2.asm.txt
+
