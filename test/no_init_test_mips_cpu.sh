a54a0b590c72ad7cc6b6f1167b5507fc31f0d500
sittingsotong <zhaositing1026@gmail.com>
2020-12-19 17:46:17 +0000

    ok it really works now

--- test/no_init_test_mips_cpu.sh
+++ test/no_init_test_mips_cpu.sh
@@ -85,6 +85,7 @@ else
         # Extract just the testcase name from the filename. See `man basename` for what this command does.
         TESTNAME=$(basename ${i} .hex.txt)
         INSTR=${TESTNAME%%_*}
+        cp ${i} test/1-binary/${INSTR}/${TESTNAME}.hex.txt
         # Dispatch to the main test-case script
         if [ ${TYPE} == 'harvard' ];then
             #echo "harvard !"
